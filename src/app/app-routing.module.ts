e3b4a00c9a380cb7927b267f3e005c9aad239adc
Muhammad Asif Adnan <asif.adnan.cse@gmail.com>
2018-04-18 07:23:12 +0600

    added route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { HeroesComponent } from './heroes/heroes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule , Routes } from '@angular/router';
+import { VillainsComponent } from './villains/villains.component';
+
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent}
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'villains', component: VillainsComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
